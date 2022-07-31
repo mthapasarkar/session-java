ae7a5d5041285555a3c08a89e3fd4c29916cab9b
susan <mthapasarkar@gmail.com>
2022-07-31 19:40:22 -0400

    Initial Commit

---
+++ src/day1/Welcome.java
@@ -0,0 +1,15 @@
+package day1;
+
+public class Welcome {
+    public static void main(String[] args) {
+        String  name  = "Welcome to java class";
+        System.out.println(name);
+        for (int i = 0; i < name.length(); i++) {
+            System.out.println("=");
+        }
+        System.out.println();
+
+        }
+
+    }
+
